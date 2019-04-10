3a0d7e12a0a0e299ddae2c8b54d613e5c657e54c
Cristian <holme027@rangers.uwp.edu>
2019-04-09 20:55:56 -0500

    Try again

--- HW3/FirstViewController.swift
+++ HW3/FirstViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    override func loadView() {
+        // Do any additional setup after loading the view, typically from a nib.
+        super.loadView()
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        
+        print(hour)
+        let lightColor = UIColor.orange
+        let darkColor = UIColor.purple
+        switch hour
+        {
+        // hours 1 to 6
+        case 1...6: self.view.backgroundColor = darkColor
+            break
+        // hours 7 to 18
+        case 7...18: self.view.backgroundColor = lightColor
+            break
+        //hours 19 to 23 and 0
+        case 19...23, 0: self.view.backgroundColor = darkColor
+            break
+        default:self.view.backgroundColor = darkColor
+        }
+    }
 }
 
--- HW3/SecondViewController.swift
+++ HW3/SecondViewController.swift
@@ -15,6 +15,28 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func loadView() {
+        super.loadView()
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        
+        print(hour)
+        let lightColor = UIColor.orange
+        let darkColor = UIColor.purple
+        switch hour
+        {
+        // hours 1 to 6
+        case 1...6: self.view.backgroundColor = darkColor
+            break
+        // hours 7 to 18
+        case 7...18: self.view.backgroundColor = lightColor
+            break
+        //hours 19 to 23 and 0
+        case 19...23, 0: self.view.backgroundColor = darkColor
+            break
+        default:self.view.backgroundColor = darkColor
+        }
+    }
 }
 
